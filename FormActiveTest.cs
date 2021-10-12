e7b71325ab271c9fea94f8eaf37b9f8122e545b2
Hristo Denev <icy.denev@gmail.com>
2021-10-12 09:01:17 +0300

    refresh project

---
+++ FormActiveTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabletcka
+{
+    public partial class FormActiveTest : Form
+    {
+        public FormActiveTest()
+        {
+            InitializeComponent();
+        }
+
+        private void FormActiveTest_Activated(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hey hey");
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            MessageBox.Show("Hey hey"); 
+        }
+    }
+}
---
+++ Services/FormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabletcka.Services
+{
+	public static class FormController
+	{
+		public static void CreateForms ()
+		{
+			Form[] forms = new Form[10];
+
+			for (int i = 0; i < 10; i ++)
+			{
+				forms[i] = new Form();
+
+				forms[i].Show();
+			}
+		}
+
+		public static void CreateForms(int numberOfWindows)
+		{
+			FormActiveTest[] forms = new FormActiveTest[numberOfWindows];
+
+			for (int i = 0; i < numberOfWindows; i++)
+			{
+				forms[i] = new FormActiveTest();
+
+				forms[i].Show();
+			}
+		}
+	}
+}
